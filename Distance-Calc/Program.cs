89314703cabfda0c14b9d2786419180c0ac905a9
EvanChhetri <112644135+EvanChhetri@users.noreply.github.com>
2023-01-13 12:24:56 -0700

    Add files via upload

---
+++ Distance-Calc/Program.cs
@@ -0,0 +1,22 @@
+﻿// Distance Calculator by Evan C
+Console.Clear();
+#nullable disable
+
+// Welcome Message
+Console.WriteLine("WELCOME TO THE DISTANCE CALCULATOR!");
+
+// Inputs
+Console.WriteLine("Enter x1 value: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter y1 value: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter x2 value: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter y2 value: ");
+double num4 = Convert.ToDouble(Console.ReadLine());
+
+// Process
+double value = Math.Sqrt((Math.Pow((num3 - num1), 2)) + (Math.Pow((num4 - num2), 2)));
+
+// Output
+Console.WriteLine($"Distance between the two points is {value}");
